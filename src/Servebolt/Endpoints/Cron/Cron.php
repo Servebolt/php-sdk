aa380a1e79a68f2cf9fb7049494ab4faf89d00ac
Robert Sæther <robert@servebolt.com>
2021-02-04 11:31:13 +0100

    Added Cron API endpoint class

---
+++ src/Servebolt/Endpoints/Cron/Cron.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Servebolt\SDK\Endpoints\Cron;
+
+use Servebolt\SDK\Traits\ApiEndpoint;
+use Servebolt\SDK\Models\CronJob;
+
+/**
+ * Class Cron
+ * @package Servebolt\SDK\Endpoints
+ */
+class Cron
+{
+
+    use ApiEndpoint;
+
+    public function list()
+    {
+    }
+
+    public function create(CronJob $cronJob)
+    {
+    }
+
+    public function get($id)
+    {
+    }
+
+    public function delete($id)
+    {
+    }
+
+    public function update(CronJob $cronJob)
+    {
+    }
+
+    public function replace(CronJob $cronJob)
+    {
+    }
+}
