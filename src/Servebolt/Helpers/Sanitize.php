bd508ed58f3acc06e3c99a3c7f9d3b4721c1bf7a
Robert Sæther <robert@servebolt.com>
2021-02-05 11:04:33 +0100

    Added scaffolding for sanitazion tests and helper functions

--- src/Servebolt/Helpers/Sanitize.php
+++ src/Servebolt/Helpers/Sanitize.php
@@ -5,10 +5,34 @@ namespace Servebolt\SDK\Helpers;
 function sanitizeUrl(string $url) : string
 {
     $urlParts = parse_url($url);
+    // TODO: Throw exception if failed
     return $url;
 }
 
-function sanitizeDomain(string $domain, bool $allowPath = true, bool $allowQueryString = false) : string
+function sanitizeUrls(array $urls) : array
 {
+    $validUrls = [];
+    foreach ($urls as $url) {
+        if (sanitizeUrl($url)) {
+            $validUrls[] = $url;
+        }
+    }
+    return $validUrls;
+}
+
+function sanitizeDomainWithPath(string $domain, bool $allowPath = true, bool $allowQueryString = false) : string
+{
+    // TODO: Throw exception if failed
     return $domain;
+}
+
+function sanitizeDomainsWithPath(array $domains, bool $allowPath = true, bool $allowQueryString = false) : array
+{
+    $validDomains = [];
+    foreach ($domains as $domain) {
+        if (sanitizeDomainWithPath($domain, $allowPath, $allowQueryString)) {
+            $validDomains[] = $domain;
+        }
+    }
+    return $validDomains;
 }
---
+++ tests/Unit/SanitizationHelperTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Servebolt\SDK\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SanitizationHelperTest extends TestCase
+{
+
+    public function testUrlSanitization()
+    {
+
+    }
+
+    /*
+    public function testDomainWithPathSanitization()
+    {
+
+    }
+
+    public function testUrlsSanitization()
+    {
+
+    }
+
+    public function testDomainsWithPathSanitization()
+    {
+
+    }
+    */
+}
